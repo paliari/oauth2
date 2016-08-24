d0aa4fd46fb650fde5225479c3ce0627f61c9a16
Marcos Paliari <marcos@paliari.com.br>
2016-08-24 11:58:53 -0300

    identa

--- src/Paliari/Oauth2ServerFacade/Storage.php
+++ src/Paliari/Oauth2ServerFacade/Storage.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Paliari\Oauth2ServerFacade;
 
 use OAuth2\Storage\Pdo;
@@ -8,20 +7,19 @@ class Storage extends Pdo
 {
 
     /**
-     * @param \Doctrine\DBAL\Connection$connection
-     * @param array $config
+     * @param \Doctrine\DBAL\Connection $connection
+     * @param array                     $config
      */
     public function __construct($connection, $config = array())
     {
-        $this->db = $connection;
-
+        $this->db     = $connection;
         $this->config = array_merge(array(
-            'client_table' => 'oauth_clients',
-            'access_token_table' => 'oauth_access_tokens',
+            'client_table'        => 'oauth_clients',
+            'access_token_table'  => 'oauth_access_tokens',
             'refresh_token_table' => 'oauth_refresh_tokens',
-            'code_table' => 'oauth_authorization_codes',
-            'user_table' => 'oauth_users',
-            'jwt_table' => 'oauth_jwt',
+            'code_table'          => 'oauth_authorization_codes',
+            'user_table'          => 'oauth_users',
+            'jwt_table'           => 'oauth_jwt',
         ), $config);
     }
 
@@ -30,7 +28,6 @@ class Storage extends Pdo
         $sql = 'SELECT * from usuarios';
         $sql .= ' where id=:user_id';
         $params = array('user_id' => $user_id);
-
         if (!$userInfo = $this->fetch($sql, $params)) {
             return false;
         }
@@ -41,8 +38,7 @@ class Storage extends Pdo
     /* OAuth2_Storage_AuthorizationCodeInterface */
     public function getAuthorizationCode($code)
     {
-        $sql = sprintf('SELECT * from %s where authorization_code = :code', $this->config['code_table']);
-
+        $sql  = sprintf('SELECT * from %s where authorization_code = :code', $this->config['code_table']);
         $code = $this->fetch($sql, compact('code'));
         if ($code) {
             // convert date string back to timestamp
@@ -55,7 +51,7 @@ class Storage extends Pdo
     /* OAuth2_Storage_ClientCredentialsInterface */
     public function checkClientCredentials($client_id, $client_secret = null)
     {
-        $sql = sprintf('SELECT * from %s where client_id = :client_id', $this->config['client_table']);
+        $sql    = sprintf('SELECT * from %s where client_id = :client_id', $this->config['client_table']);
         $result = $this->fetch($sql, compact('client_id'));
 
         // make this extensible
@@ -72,8 +68,7 @@ class Storage extends Pdo
     /* OAuth2_Storage_AccessTokenInterface */
     public function getAccessToken($access_token)
     {
-        $sql = sprintf('SELECT * from %s where access_token = :access_token', $this->config['access_token_table']);
-
+        $sql   = sprintf('SELECT * from %s where access_token = :access_token', $this->config['access_token_table']);
         $token = $this->fetch($sql, compact('access_token'));
         if ($token) {
             // convert date string back to timestamp
@@ -86,10 +81,8 @@ class Storage extends Pdo
     /* OAuth2_Storage_RefreshTokenInterface */
     public function getRefreshToken($refresh_token)
     {
-        $sql = sprintf('SELECT * from %s where refresh_token = :refresh_token', $this->config['refresh_token_table']);
-
+        $sql   = sprintf('SELECT * from %s where refresh_token = :refresh_token', $this->config['refresh_token_table']);
         $token = $this->fetch($sql, compact('refresh_token'));
-
         if ($token) {
             // convert expires to epoch time
             $token['expires'] = strtotime($token['expires']);
@@ -98,11 +91,10 @@ class Storage extends Pdo
         return $token;
     }
 
-    public function fetch($sql, $params=array())
+    public function fetch($sql, $params = array())
     {
         $stmt = $this->db->prepare($sql);
         $stmt->execute($params);
-
         if (!$res = $stmt->fetch(\PDO::FETCH_ASSOC)) {
             return false;
         }
@@ -116,7 +108,7 @@ class Storage extends Pdo
     public function gc()
     {
         $expires = date('Y-m-d H:i:s');
-        $tables = array(
+        $tables  = array(
             'access_token_table',
             'refresh_token_table',
             'code_table',
@@ -126,4 +118,5 @@ class Storage extends Pdo
             $stmt->execute(compact('expires'));
         }
     }
+
 }
